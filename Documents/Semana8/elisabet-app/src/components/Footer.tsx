89aea2e19361413c23519e9d432c9a15cfe13b37
ElisabetT <elisabetfiorella@hotmail.com>
2025-05-27 10:10:19 +0200

    Primer commit: Creadas las 3 páginas básico, y los componentes reutilizables.

--- Documents/Semana8/elisabet-app/src/components/Footer.tsx
+++ Documents/Semana8/elisabet-app/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="w-full bg-balck-100 text-center p-4 mt-10 text-sm text-gray-600">
+        <footer>
             <p>© {new Date().getFullYear()} Elisabet Torres.</p>
         </footer>
     );
--- Documents/Semana8/elisabet-app/src/components/Header.tsx
+++ Documents/Semana8/elisabet-app/src/components/Header.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="w-full bg-black-100 p-4 shadow-md">
-            <nav className="flex gap-4 justify-center">
+        <header>
+            <nav>
                 <Link href="/">Inicio</Link>
                 <Link href="/about">Sobre mí</Link>
                 <Link href="/contact">Contacto</Link>
